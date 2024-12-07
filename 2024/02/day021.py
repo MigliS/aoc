efe631b71ae3b89907bfdbae542f044531578eff
Michael Stucki <michael@rack.farm>
2024-12-07 16:48:08 +0100

    split line into numbers

--- 2024/02/day021.py
+++ 2024/02/day021.py
@@ -2,4 +2,7 @@ with open("input.txt", "r") as file:
     lines = file.readlines()
 
 for line in lines:
-    print("Line:", line)
+    # print("Line:", line)
+    numbers = line.split()
+
+    print("Numbers:", numbers)
