079a194e8441b7e545c1ba53d72174d643f56c84
Michael Stucki <michael@rack.farm>
2024-12-07 15:51:45 +0100

    day01

--- 2024/01/day01.py
+++ 2024/01/day01.py
@@ -1,4 +1,26 @@
-def main():
-    file = open("input.txt", "r")
-    content = file.read()
-    print(content)
+with open("input.txt", "r") as file:
+    data = file.read()
+
+rows = data.strip().split("\n")
+
+left_list = []
+right_list = []
+
+for row in rows:
+    left_value, right_value = map(int, row.split())
+    left_list.append(left_value)
+    right_list.append(right_value)
+
+# ll = min(left_list)
+# lr = min(right_list)
+left_list.sort()
+right_list.sort()
+
+differences = []
+
+for l, r in zip(left_list, right_list):
+    difference = l - r
+    differences.append(difference)
+
+
+print(sum(differences))
